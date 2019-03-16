ac03a9e5a9307ca1afba46459d93e61ff0d0712e
Austin Manansala <Austindm1994@gmail.com>
2019-03-15 23:41:51 -0400

    Front-End Start

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+import {  RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'register',
+    component: RegistrationComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes),
+   CommonModule ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
