166af84966338db95dca56559e145f77bc977e12
Ryan Lockard <jesselockard@gmail.com>
2019-04-05 13:07:32 -0400

    Fixed GameComponent's declaration syntax

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,18 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
-Component({
+@Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.css']
-});
-
-const config: GameConfig = {
-  title: 'CodeQuest',
-  width: 800,
-  height: 600,
-  parent: 'game',
-  backgroundColor: '#18216D',
-};
-
+})
 export class GameComponent {
+  const config: GameConfig = {
+    title: 'CodeQuest',
+    width: 800,
+    height: 600,
+    parent: 'game',
+    backgroundColor: '#18216D',
+  };
+
+  constructor() {}
 }
