2c28a0cca61d670fd91e53f9c93ac81cf4abf061
Ryan Lockard <jesselockard@gmail.com>
2019-04-05 13:03:54 -0400

    Added the NavBar component, PhaserModule, and FlexLayoutModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+// Angular Routing
 import { AppRoutingModule } from './app-routing.module';
+
+// Phaser module
+import { PhaserModule } from 'phaser-component-library';
+
+// Flexlayout Modules
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+// Application Component's
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+
+// Authentication components
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
-import { UserService } from './services/user.service';
+
+// Game and Leaderboard components
 import { LeaderboardComponent } from './leaderboard/leaderboard.component';
 import { GameComponent } from './game/game.component';
+
+// Application Service's
+import { UserService } from './services/user.service';
+import { NavbarComponent } from './navbar/navbar.component';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,11 +33,14 @@ import { GameComponent } from './game/game.component';
     LoginComponent,
     RegistrationComponent,
     LeaderboardComponent,
-    GameComponent
+    GameComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PhaserModule,
+    FlexLayoutModule
   ],
   providers: [ UserService ],
   bootstrap: [AppComponent]
