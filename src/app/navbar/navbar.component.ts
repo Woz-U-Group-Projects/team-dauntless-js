697bfd22e2205a31af8a650e7334d5582b622703
Ryan Lockard <jesselockard@gmail.com>
2019-04-05 15:17:17 -0400

    Added navigation and removed global margin styling

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'block-navbar',
+  selector: 'navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
