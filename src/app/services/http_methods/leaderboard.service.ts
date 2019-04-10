b955189e1020e4bc24e3c0dbc627cb189171c817
Ryan Lockard <jesselockard@gmail.com>
2019-04-10 15:34:24 -0400

    Added method for retrieving full leaderboard list using the GET /leaderboard/ route

--- src/app/services/http_methods/leaderboard.service.ts
+++ src/app/services/http_methods/leaderboard.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { Leaderboard } from '../../models/leaderboard';
+
 @Injectable({
   providedIn: 'root'
 })
 export class LeaderboardService {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
 
-  
+  getLeaderboardList() : Observable<any> {
+    let header = new HttpHeaders({"Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6InJ5YW4ifQ.tYg449BU5dDH-PlnRftoDxWAfkfA-6kAe6MxWsoyiB8"});
+
+    return this.http.get("http://localhost:8080/leaderboard", {headers: header});
+  }
 }
