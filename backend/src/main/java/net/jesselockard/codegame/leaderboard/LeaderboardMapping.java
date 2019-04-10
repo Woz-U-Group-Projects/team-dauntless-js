587c9b8dcc81f1693adc5b589b03eb03746ba2fb
Ryan Lockard <jesselockard@gmail.com>
2019-04-10 12:38:32 -0400

    Added @GetMapping for the /leaderboard/ route to return a full list of all user's and their scores

--- backend/src/main/java/net/jesselockard/codegame/leaderboard/LeaderboardMapping.java
+++ backend/src/main/java/net/jesselockard/codegame/leaderboard/LeaderboardMapping.java
@@ -1,6 +1,7 @@
 package net.jesselockard.codegame.leaderboard;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -41,6 +42,8 @@ public class LeaderboardMapping {
 	
 	/*
 	 * getUserLeaderboard provides a user's (identified by the JWT passed through the header) leaderboard information
+	 * 
+	 * Retrieves a SINGLE leaderboard document.
 	 */
 	@GetMapping(path = "/leaderboard/user", produces = {MediaType.APPLICATION_JSON_VALUE})
 	Map<String, Object> getUserLeaderboard(@RequestHeader(value="Authorization") String JWTUserToken) {
@@ -112,10 +115,48 @@ public class LeaderboardMapping {
 		return leaderboardResponse;
 	}
 	
-	/*
-	 * putUserLeaderboard takes a leaderboard structure from the request body and updates or creates a
-	 * userboard belonging to the JWT's claimed user.
-	 */
+	@GetMapping(path = "/leaderboard", produces = { MediaType.APPLICATION_JSON_VALUE })
+	Map<String, Object> getLeaderboard(@RequestHeader(value="Authorization") String JWTUserToken) {
+		String token = JWTUserToken.replace("Bearer ", "");
+		
+		/*
+		 * Use the JWTHandler to parse the token and ensure the user has been
+		 * truly previously authenticated.
+		 * 
+		 * jwt.verifyUserToken will return null if the tokens signiture or claims are incorrect.
+		 * The signiture should always be correct as it's defined within the JWTHandler class.
+		 * The only claim provided should be within the payload as 'username' which is set to the
+		 * player's username.
+		 */
+		JWTHandler jwt = new JWTHandler();
+		DecodedJWT decodedToken = jwt.verifyUserToken(token);
+		
+		Map<String, Object> leaderboardResponse = new HashMap<String, Object>();
+
+		if(decodedToken != null) {
+			List<Leaderboard> userBoards = lbRepository.findAll();
+			
+			if(!userBoards.isEmpty()) {
+				userBoards.forEach((leaderboard)-> {
+					leaderboardResponse.put(leaderboard.username, leaderboard);
+				});
+			} else {
+				leaderboardResponse.put("error", "No user's were found in the database.");
+			}
+		}
+		
+		else {
+			/*
+			 * Utilize the leaderboardResponse hash map to return the token used and
+			 * inform the frontend there was an error trying to authenticate the token.
+			 */
+			leaderboardResponse.put("userToken", token);
+			leaderboardResponse.put("error", "There was an error authenticated the user's token.");
+		}
+		
+		return leaderboardResponse;
+	}
+	
 	@PostMapping(path = "/leaderboard/user", produces = {MediaType.APPLICATION_JSON_VALUE})
 	Map<String, Object> putUserLeaderboard(@RequestHeader(value="Authorization") String JWTUserToken, @RequestBody LeaderboardPutRequest leaderboardData) {
 		String token = JWTUserToken.replace("Bearer ", "");
